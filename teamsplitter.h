a8100886986adb803472284ca50e3aab7c2267d4
ykhazimx <yanx.khazimov@intel.com>
2018-11-19 17:01:58 +0300

    best split animated

--- teamsplitter.h
+++ teamsplitter.h
@@ -6,19 +6,16 @@
 class TeamSplitter: public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QStringList homeRoster MEMBER m_homeRoster NOTIFY changed)
-    Q_PROPERTY(QStringList awayRoster MEMBER m_awayRoster NOTIFY changed)
+    Q_PROPERTY(QList<QVariant> splitVariants MEMBER m_splitVariants)
 
 public:
     TeamSplitter();
 
-    Q_INVOKABLE void split(QStringList home, QStringList away, QStringList unsorted);
-
-signals:
-    void changed();
+    Q_INVOKABLE void split(QStringList home, QStringList away, QStringList unsorted, int maxResults);
 
 private:
-    QStringList m_homeRoster, m_awayRoster;
+    QList<QVariant> m_splitVariants;
+    void pick(int picksToGo, QStringList home, const QStringList& away, QStringList unsorted, int pickFrom = 0);
 };
 
 #endif // TEAMSPLITTER_H
